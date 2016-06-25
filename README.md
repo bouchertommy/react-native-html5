0412b38f71fa49c7d2ae317f53ffbfe26ff02b1d
Pillys <pillys@163.com>
2016-06-25 17:29:02 +0800

    v0.2.0

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # react-native-html5
 html, html5 parser for react-native
 
+![Mou icon](https://pillys.github.io/react-native-html5/images/0.png)
+![Mou icon](https://pillys.github.io/react-native-html5/images/1.png)
+
+## Install
+```
+npm install react-native-html5
+```
+
+## Usage
+
+### rawHtml
+
+HTML Text
+
+### styleSheet
+
+optional, styles
+
+### externalStyleSheet
+
+optional, external styles, textIndent for &lt;p&gt; supported
+
 ## Example
 ```
   import React, { Component } from 'react';
@@ -14,28 +36,50 @@ html, html5 parser for react-native
 
   const rawHtml = `
   <div>
-    <p>Hello,  I'm react-native-html5.</p>
-    <p><a href="http://www.qque.com">cool!</a></p>
+    <h1>h1. heading</h1>
+    <h2>h2. heading</h2>
+    <h3>h3. heading</h3>
+    <h4>h4. heading</h4>
+    <h5>h5. heading</h5>
+    <h6>h6. heading</h6>
+    <p>&lt;p&gt; is a paragraph</p>
+    <p>&lt;image&gt; rendered as an image</p>
     <img src="https://facebook.github.io/react/img/logo_og.png" />
-    <p>sdf&nbsp;&nbsp;&copy;hello,&copy;</p>
+    <p>&lt;mark&gt; renderd as <mark>highlight text</mark></p>
+    <p>&lt;del&gt; or &lt;s&gt; rendered as <del>deleted text</del> </p>
+    <p>&lt;u&gt; or &lt;ins&gt; rendered as <u>underlined text</u></p>
+    <p>&lt;small&gt; rendered as <small>small text</small></p>
+    <p>&lt;strong&gt; or &lt;b&gt; <strong>rendered as bold text</strong></p>
+    <p>&lt;em&gt; or &lt;i&gt; <em>rendered as italicized text</em></p>
+    <blockquote>
+      <p>&lt;blockquote&gt; rendered as a blockquote</p>
+    </blockquote>
+    <p>&lt;code&gt; <code>rendered as code</code></p>
+    <p>and other most of html tags supported.</p>
   </div>
   `;
-
+  
   const styleSheet = {
     a: {
       color: '#00f',
     }
   };
+  const externalStyleSheet = {
+    p: {
+      textIndent: 0,
+    },
+  };
 
   class ReactNativeHtml5Test extends Component {
     render() {
       return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
           <Html5
             rawHtml={rawHtml}
             styleSheet={styleSheet}
+            externalStyleSheet={externalStyleSheet}
           />
-        </View>
+        </ScrollView>
       );
     }
   }
@@ -43,6 +87,7 @@ html, html5 parser for react-native
   const styles = StyleSheet.create({
     container: {
       marginTop: 40,
+      padding: 20,
     },
   });
 ```
--- index.js
+++ index.js
@@ -11,8 +11,39 @@ import {
 import htmlparser from './lib/htmlparser';
 
 const ratio = PixelRatio.get();
+const InlineTags = ['b','font','i','em','big','strong','small','sub','sup','u','ins','mark','code','address','del','s','strike','a','label','span', 'br'];
 
 const defaultStyle = {
+  h1: {
+    fontWeight: '500',
+    fontSize: 36,
+    marginVertical: 5,
+  },
+  h2: {
+    fontWeight: '500',
+    fontSize: 30,
+    marginVertical: 5,
+  },
+  h3: {
+    fontWeight: '500',
+    fontSize: 24,
+    marginVertical: 5,
+  },
+  h4: {
+    fontWeight: '500',
+    fontSize: 18,
+    marginVertical: 5,
+  },
+  h5: {
+    fontWeight: '500',
+    fontSize: 14,
+    marginVertical: 5,
+  },
+  h6: {
+    fontWeight: '500',
+    fontSize: 12,
+    marginVertical: 10,
+  },
   a: {
     color: '#007aff',
     fontWeight: '500',
@@ -23,6 +54,60 @@ const defaultStyle = {
   strong: {
     fontWeight: 'bold',
   },
+  i: {
+    fontStyle: 'italic',
+  },
+  em: {
+    fontStyle: 'italic',
+  },
+  big: {
+    fontSize: 16,
+  },
+  small: {
+    fontSize: 10,
+  },
+  u: {
+    textDecorationLine: 'underline',
+    textDecorationStyle: 'solid',
+  },
+  ins: {
+    textDecorationLine: 'underline',
+    textDecorationStyle: 'solid',
+  },
+  del: {
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid',
+  },
+  s: {
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid',
+  },
+  strike: {
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid',
+  },
+  mark: {
+    backgroundColor: '#fcf8e3',
+    borderRadius: 3,
+  },
+  code: {
+    fontFamily: 'Menlo',
+    color: '#c9314e',
+    backgroundColor: '#faf3f4',
+    borderRadius: 3,
+  },
+  blockquote: {
+    borderLeftWidth: 5,
+    borderLeftColor: '#eeeeee',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
+};
+
+const defaultExternalStyle = {
+  p: {
+    textIndent: 0,
+  }
 };
 
 export default class Html5 extends Component {
@@ -63,9 +148,11 @@ export default class Html5 extends Component {
 
   compile(doms) {
     var styleSheet = this.props.styleSheet;
+    var externalStyleSheet = this.props.externalStyleSheet;
     return doms.map((dom, index) => {
       if(dom.type === 'tag') {
         let style = Object.assign({}, defaultStyle[dom.name], styleSheet[dom.name]);
+        let externalStyle = Object.assign({}, defaultExternalStyle[dom.name], externalStyleSheet[dom.name]);
         switch(dom.name) {
           case 'img':
             if(dom.attribs.width) {
@@ -82,10 +169,31 @@ export default class Html5 extends Component {
                 resizeMode={Image.resizeMode.strech}
               />
             );
-          case 'span':
-          case 'time':
+          case 'font':
+          case 'i':
+          case 'em':
           case 'b':
           case 'strong':
+          case 'big':
+          case 'small':
+          case 'sub':
+          case 'sup':
+          case 'mark':
+          case 'code':
+          case 'u':
+          case 'ins':
+          case 'del':
+          case 's':
+          case 'strike':
+          case 'span':
+          case 'time':
+          case 'label':
+          case 'h1':
+          case 'h2':
+          case 'h3':
+          case 'h4':
+          case 'h5':
+          case 'h6':
             return (
               <Text
                 key={index}
@@ -105,12 +213,43 @@ export default class Html5 extends Component {
               <Text key={index}>{'\n'}</Text>
             );
           default:
-            return (
-              <View
-                key={index}
-                style={style}
-              >{this.compile(dom.children)}</View>
-            );
+            let blockChild = dom.children.find(child => {
+              return child.type !== 'text' && !InlineTags.includes(child.name);
+            });
+            //\u3000
+            if(blockChild) {
+              return (
+                <View
+                  key={index}
+                  style={style}
+                >{this.compile(dom.children)}</View>
+              );
+            } else {
+              if(dom.name === 'p') {
+                return (
+                  <View
+                    key={index}
+                    style={style}
+                  >
+                    <Text>
+                      <Text>{' '.repeat(externalStyle.textIndent)}</Text>
+                      {this.compile(dom.children)}
+                    </Text>
+                  </View>
+                );
+              } else {
+                return (
+                  <View
+                    key={index}
+                    style={style}
+                  >
+                    <Text>
+                      {this.compile(dom.children)}
+                    </Text>
+                  </View>
+                );
+              }
+            }
         }
       } else if(dom.type === 'text') {
         return (
